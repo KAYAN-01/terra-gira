906a44c259b6a1df55e41adc20cf6fa4c1fa5afb
KAYAN-01 <guimaraes.kayan@escola.pr.gov.br>
2026-03-11 15:20:07 -0300

    sketch123

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
+let terra;
+
 function setup(){
-    createCanvas(1920, 1080,WEGL); //plano 3D
     terra=loadImage('terrajpg')
 }
-function draw( {
+function setup() {
+    createCanvas(1920,1080, WEBGL);//plano  3D
+}
+
+
+function draw(){
 background(205, 102, 94);//cor de fundo
 rotateY(millis()/1000);//rotação da terra
-TextTrackCue(terra);//textura da terra, imagem.
-Spere(80, 100);//esfera 3D
+texture(terra);//textura da terra, imagem.
+Sphere(80, 100);//esfera 3D
+
 }
